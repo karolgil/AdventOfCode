a0a8b9bbba24b5712fc42c8a8eae11698913d3b2
karolgil <karol.gil@getbase.com>
2018-12-03 08:58:32 +0100

    Solution to part 2 of day 2

--- 2018/task2/task.go
+++ 2018/task2/task.go
@@ -1,13 +1,15 @@
 package task1
 
 import (
+	"errors"
+
 	"github.com/karolgil/AdventOfCode/2018/utils"
 )
 
 func Solution1(inputFile string) (int, error) {
 	lines, err := utils.ReadLinesFrom(inputFile)
 	if err != nil {
-		return 0, nil
+		return 0, err
 	}
 
 	var twos int
@@ -53,4 +55,41 @@ func alphabet() string {
 		b[i] = 'a' + byte(i)
 	}
 	return string(b)
+}
+
+func Solution2(inputFile string) (string, error) {
+	lines, err := utils.ReadLinesFrom(inputFile)
+	if err != nil {
+		return "", err
+	}
+
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < len(lines); j++ {
+			if i == j {
+				break
+			}
+			str1 := lines[i]
+			str2 := lines[j]
+			diffCount := 0
+			for k := 0; k < len(str1); k++ {
+				if str1[k] != str2[k] {
+					diffCount += 1
+				}
+				if diffCount > 1 {
+					break
+				}
+			}
+			if diffCount == 1 {
+				var result string
+				for k := 0; k < len(str1); k++ {
+					if str1[k] == str2[k] {
+						result += string(str1[k])
+					}
+				}
+				return result, nil
+			}
+		}
+	}
+
+	return "", errors.New("could not find result")
 }
--- 2018/task2/task_test.go
+++ 2018/task2/task_test.go
@@ -31,3 +31,29 @@ func BenchmarkSolution1(b *testing.B) {
 		Solution1("input1")
 	}
 }
+
+func TestSolution2(t *testing.T) {
+	for _, test := range []struct {
+		inputFile string
+		expected  string
+	}{
+		{
+			inputFile: `input1`,
+			expected:  "pbykrmjmizwhxlqnasfgtycdv",
+		},
+		{
+			inputFile: `input3`,
+			expected:  "fgij",
+		},
+	} {
+		solution, err := Solution2(test.inputFile)
+		assert.NoError(t, err, "Solution2 should not return any errors")
+		assert.Equal(t, test.expected, solution, "should be equal")
+	}
+}
+
+func BenchmarkSolution2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solution2("input1")
+	}
+}
