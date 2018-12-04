7e916450da8df3cb7f307768787183fccfe09ce9
karolgil <karol.gil@getbase.com>
2018-12-04 13:23:56 +0100

    Solution to part 2 of day 4

--- 2018/task4/task.go
+++ 2018/task4/task.go
@@ -13,86 +13,15 @@ func Solution1(inputFile string) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-
-	guardEntersRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+)\].*#([0-9]+) .*$`)
-	fallAsleepRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+)\].*falls.*$`)
-	wakeUpRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+)\].*wakes.*$`)
-	layout := "2006-01-02 15:04"
-	guardCalendar := make(map[time.Time]*daySchedule)
-
-	for _, line := range lines {
-		guardEntry := guardEntersRegex.FindAllStringSubmatch(line, -1)
-		if len(guardEntry) > 0 {
-			dateStr := guardEntry[0][1]
-			guardID := guardEntry[0][2]
-			date, err := time.Parse(layout, dateStr)
-			if err != nil {
-				return 0, err
-			}
-			date = date.Add(1 * time.Hour).Truncate(24 * time.Hour)
-			_, exists := guardCalendar[date]
-			if !exists {
-				guardCalendar[date] = newDaySchedule()
-			}
-			id, err := strconv.Atoi(guardID)
-			if err != nil {
-				return 0, err
-			}
-			guardCalendar[date].id = id
-		} else {
-			fallAsleep := fallAsleepRegex.FindAllStringSubmatch(line, -1)
-			if len(fallAsleep) > 0 {
-				dateStr := fallAsleep[0][1]
-				date, err := time.Parse(layout, dateStr)
-				if err != nil {
-					return 0, err
-				}
-				minute := date.Minute()
-				date = date.Truncate(24 * time.Hour)
-				_, exists := guardCalendar[date]
-				if !exists {
-					guardCalendar[date] = newDaySchedule()
-				}
-				guardCalendar[date].events[minute] = 1
-			} else {
-				wakesUp := wakeUpRegex.FindAllStringSubmatch(line, -1)
-				dateStr := wakesUp[0][1]
-				date, err := time.Parse(layout, dateStr)
-				if err != nil {
-					return 0, err
-				}
-				minute := date.Minute()
-				date = date.Truncate(24 * time.Hour)
-				_, exists := guardCalendar[date]
-				if !exists {
-					guardCalendar[date] = newDaySchedule()
-				}
-				guardCalendar[date].events[minute] = 0
-			}
-		}
+	guardSleepSchedules, err := getGuardsSleepSchedules(lines)
+	if err != nil {
+		return 0, err
 	}
-	longest := make(map[int]*longestSleep)
-	for _, schedule := range guardCalendar {
-		if _, exists := longest[schedule.id]; !exists {
-			longest[schedule.id] = &longestSleep{schedules: [][]int{}}
-		}
-		asleep := 0
-		for i := 0; i < 60; i++ {
-			if newState, exists := schedule.events[i]; exists {
-				asleep = newState
-			}
-			schedule.midnightSchedule = append(schedule.midnightSchedule, asleep)
-			if asleep == 1 {
-				longest[schedule.id].sum += 1
-			}
-		}
-		currentSchedules := longest[schedule.id].schedules
-		longest[schedule.id].schedules = append(currentSchedules, schedule.midnightSchedule)
-	}
+
 	bestSum := 0
 	bestID := 0
 	var bestSchedules [][]int
-	for id, current := range longest {
+	for id, current := range guardSleepSchedules {
 		if current.sum > bestSum {
 			bestSum = current.sum
 			bestID = id
@@ -116,18 +45,125 @@ func Solution1(inputFile string) (int, error) {
 	return bestID * bestMinute, nil
 }
 
-func bToI(b bool) int {
-	if b {
-		return 1
+func Solution2(inputFile string) (int, error) {
+	lines, err := utils.ReadLinesFrom(inputFile)
+	if err != nil {
+		return 0, err
 	}
-	return 0
+	guardSleepSchedules, err := getGuardsSleepSchedules(lines)
+	if err != nil {
+		return 0, err
+	}
+
+	bestMinute := -1
+	bestAsleepCount := 0
+	bestID := 0
+	for id, current := range guardSleepSchedules {
+		for i := 0; i < 60; i++ {
+			currentAsleepCount := 0
+			for _, schedule := range current.schedules {
+				currentAsleepCount += schedule[i]
+			}
+			if currentAsleepCount > bestAsleepCount {
+				bestMinute = i
+				bestAsleepCount = currentAsleepCount
+				bestID = id
+			}
+		}
+	}
+	return bestID * bestMinute, nil
+}
+
+func getGuardsSleepSchedules(inputLines []string) (map[int]*longestSleep, error) {
+	guardsSchedules, err := getGuardsCalendar(inputLines)
+	if err != nil {
+		return nil, err
+	}
+
+	guardsSleepSchedules := make(map[int]*longestSleep)
+	for _, schedule := range guardsSchedules {
+		if _, exists := guardsSleepSchedules[schedule.id]; !exists {
+			guardsSleepSchedules[schedule.id] = newLongestSleep()
+		}
+		asleep := 0
+		for i := 0; i < 60; i++ {
+			if newState, exists := schedule.events[i]; exists {
+				asleep = newState
+			}
+			schedule.midnightSchedule = append(schedule.midnightSchedule, asleep)
+			if asleep == 1 {
+				guardsSleepSchedules[schedule.id].sum += 1
+			}
+		}
+		currentSchedules := guardsSleepSchedules[schedule.id].schedules
+		guardsSleepSchedules[schedule.id].schedules = append(currentSchedules, schedule.midnightSchedule)
+	}
+	return guardsSleepSchedules, nil
+}
+
+func getGuardsCalendar(inputLines []string) (map[time.Time]*daySchedule, error) {
+	guardEntersRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+).*#([0-9]+) .*$`)
+	fallAsleepRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+).*falls.*$`)
+	wakeUpRegex := regexp.MustCompile(`^\[([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+).*wakes.*$`)
+	layout := "2006-01-02 15:04"
+	guardCalendar := make(map[time.Time]*daySchedule)
+
+	for _, line := range inputLines {
+		guardEntry := guardEntersRegex.FindAllStringSubmatch(line, -1)
+		if len(guardEntry) > 0 {
+			dateStr := guardEntry[0][1]
+			guardID := guardEntry[0][2]
+			date, err := time.Parse(layout, dateStr)
+			if err != nil {
+				return nil, err
+			}
+			date = date.Add(1 * time.Hour).Truncate(24 * time.Hour)
+			initDate(guardCalendar, date)
+			id, err := strconv.Atoi(guardID)
+			if err != nil {
+				return nil, err
+			}
+			guardCalendar[date].id = id
+		} else {
+			fallAsleep := fallAsleepRegex.FindAllStringSubmatch(line, -1)
+			if len(fallAsleep) > 0 {
+				dateStr := fallAsleep[0][1]
+				date, err := time.Parse(layout, dateStr)
+				if err != nil {
+					return nil, err
+				}
+				minute := date.Minute()
+				date = date.Truncate(24 * time.Hour)
+				initDate(guardCalendar, date)
+				guardCalendar[date].events[minute] = 1
+			} else {
+				wakesUp := wakeUpRegex.FindAllStringSubmatch(line, -1)
+				dateStr := wakesUp[0][1]
+				date, err := time.Parse(layout, dateStr)
+				if err != nil {
+					return nil, err
+				}
+				minute := date.Minute()
+				date = date.Truncate(24 * time.Hour)
+				initDate(guardCalendar, date)
+				guardCalendar[date].events[minute] = 0
+			}
+		}
+	}
+	return guardCalendar, nil
+}
+
+func initDate(guardCalendar map[time.Time]*daySchedule, date time.Time) {
+	_, exists := guardCalendar[date]
+	if !exists {
+		guardCalendar[date] = newDaySchedule()
+	}
 }
 
 type daySchedule struct {
 	id               int
 	events           map[int]int
 	midnightSchedule []int
-	length           int
 	sum              int
 }
 
@@ -141,4 +177,10 @@ func newDaySchedule() *daySchedule {
 type longestSleep struct {
 	sum       int
 	schedules [][]int
+}
+
+func newLongestSleep() *longestSleep {
+	return &longestSleep{
+		schedules: [][]int{},
+	}
 }
--- 2018/task4/task_test.go
+++ 2018/task4/task_test.go
@@ -31,3 +31,29 @@ func BenchmarkSolution1(b *testing.B) {
 		Solution1("input1")
 	}
 }
+
+func TestSolution2(t *testing.T) {
+	for _, test := range []struct {
+		inputFile string
+		expected  int
+	}{
+		{
+			inputFile: `input1`,
+			expected:  97884,
+		},
+		{
+			inputFile: `input2`,
+			expected:  4455,
+		},
+	} {
+		solution, err := Solution2(test.inputFile)
+		assert.NoError(t, err, "Solution2 should not return any errors")
+		assert.Equal(t, test.expected, solution, "should be equal")
+	}
+}
+
+func BenchmarkSolution2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solution2("input1")
+	}
+}
